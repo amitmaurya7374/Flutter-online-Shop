24a06661e18ea3df2095fbbae7dd668cb2c4b38f
amit <amit@pop-os.localdomain>
2020-08-17 16:13:53 +0530

    added some ui  styling

---
+++ lib/widgets/productitem.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+  ProductItem({
+    this.id,
+    this.imageUrl,
+    this.title,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12.0),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black45,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          leading: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.deepOrange,
+            ),
+            onPressed: () => print('show cart page'),
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.favorite_border,
+              color: Colors.deepOrange,
+            ),
+            onPressed: () => print('Liked that item'),
+          ),
+        ),
+      ),
+    );
+  }
+}
