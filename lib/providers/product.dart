bc1d3ea4752dd0fb1da6b3a04f0485cc228312d1
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-08-25 11:34:14 +0530

    added favorite to databases

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,30 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _changeFavoriteStatus(newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite; //storing this for  error handling
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = 'https://online-shop-f007e.firebaseio.com/products/$id.';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode(
+          {
+            'isFavorite': isFavorite,
+          },
+        ),
+      ); //updating is favorites
+      if (response.statusCode >= 400) {
+        _changeFavoriteStatus(oldStatus);
+      }
+    } catch (error) {
+      _changeFavoriteStatus(oldStatus);
+    }
   }
 }
